04c822925fe57597b27fea3254783c1df97f82d2
wallace <exnesswallace@gmail.com>
2023-06-25 11:58:58 -0700

    conversion specifiers: c,s,%

---
+++ character_string.c
@@ -0,0 +1,58 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character string
+ * Return: num
+ */
+
+int _printf(const char *format, ...)
+{
+	int num = 0;
+	char c;
+	char *s;
+
+	va_list args;
+	
+	va_start(args, format);
+	
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			 if (*format == 'c')
+			 {
+				 c = (char)va_arg(args, int);
+				 _putchar(c);
+				 num++;
+			 }
+			 else if (*format == 's')
+			 {
+				 s = va_arg(args, char*);
+				 while (*s != '\0')
+				 {
+					 _putchar(*s);
+					 s++;
+					 num++;
+				 }
+			 }
+			 else if (*format == '%')
+			{
+				_putchar('%');
+				num++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			num++;
+		}
+	format++;
+	}
+	va_end(args);
+	
+	return (num);
+}
