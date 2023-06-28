b925c2cfb0601d34e10fd2cf5916970ad653a684
wallace <exnesswallace@gmail.com>
2023-06-27 17:48:06 -0700

    re restart

--- my_print.c
+++ my_print.c
@@ -1,33 +1,13 @@
-#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * my_print - Handles i and d conversion specifiers
- * @format: character string
- * Return: void
+ * print - prints numbers one by one
+ * @i: the munber
+ * Return: number
  */
 
-void my_print(const char *format, ...)
+int print(int i)
 {
-	int d, i = 0;
-	va_list args;
-
-	va_start(args, format);
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
-		{
-			d = va_arg(args, int);
-			_putchar(d / 10 + '0');
-			_putchar(d % 10 + '0');
-			i += 2;
-		}
-		else
-		{
-			_putchar(format[i]);
-			i++;
-		}
-	}
-
-	va_end(args);
+	return (write(1, &i, 1));
 }
